22dcb450f693c3409b58e0df86b9e7ae5fd543b0
subas45 <poudel.subas089@gmail.com>
2017-02-26 21:45:18 +0545

    added devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable, :registerable,
+  devise :invitable, :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,  :authentication_keys => [:login]
 
   ########################################
