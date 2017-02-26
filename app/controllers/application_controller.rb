84567d391b49f9ce88a8e84f44f3e07f8e1158ee
subas45 <poudel.subas089@gmail.com>
2017-02-26 15:59:32 +0545

    added functionality to allow username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Callbacks
+  before_action :authenticate_user!, :unless => :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :setup_user_for_system, :unless => :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def setup_user_for_system
+    # check if the user has temp password
+    # if yes force user to change password
+    if user_signed_in? && current_user.temp_password.present?
+      redirect_to edit_user_registration_path, alert: "You must change your password before logging in for the first time"
+    end
+  end
 end
