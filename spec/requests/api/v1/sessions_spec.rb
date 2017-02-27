1f451012f8ca327cdcfb400c1b30739271de95f2
subas45 <poudel.subas089@gmail.com>
2017-02-27 15:05:21 +0545

    specs for sessions

---
+++ spec/requests/api/v1/sessions_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::SessionsController, type: :request do
+  let(:user) {create(:user)}
+
+  it "creates a Session" do
+    post "/api/v1/users/sign_in", params: { user: {:login => user.email, password: user.password }}
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:ok)
+    expect(response.headers['Authorization']).to_not be_blank
+    json = JSON.parse(response.body)
+    expect(json['email']).to eq user.email
+  end
+
+  it "destroys a Session" 
+end
