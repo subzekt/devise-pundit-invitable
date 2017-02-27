16cc41348338a0bfbbbcca7410ea62772b894f8d
subas45 <poudel.subas089@gmail.com>
2017-02-27 15:15:52 +0545

    added spec for requests

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -3,6 +3,21 @@ require "rails_helper"
 RSpec.describe Api::V1::SessionsController, type: :request do
   let(:user) {create(:user)}
 
+  context "invalid logins" do
+    it "returns error on wrong password" do
+      post "/api/v1/users/sign_in", params: { user: {:login => user.email, password: 'rand' }}
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "returns error on wrong login" do
+      post "/api/v1/users/sign_in", params: { user: {:login => 'tsss', password: user.password }}
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+  end
+
   it "creates a Session" do
     post "/api/v1/users/sign_in", params: { user: {:login => user.email, password: user.password }}
     expect(response.content_type).to eq("application/json")
@@ -12,5 +27,7 @@ RSpec.describe Api::V1::SessionsController, type: :request do
     expect(json['email']).to eq user.email
   end
 
-  it "destroys a Session" 
+
+
+  it "destroys a Session"
 end
