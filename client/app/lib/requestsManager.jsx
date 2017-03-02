d8ac0dc567c327a7c52d5b7a8ee1e0b9fa40392e
subas45 <poudel.subas089@gmail.com>
2017-03-03 00:28:30 +0545

    added minor lib utility for axis fetching

---
+++ client/app/lib/requestsManager.jsx
@@ -0,0 +1,37 @@
+import request from 'axios';
+const BASE_URL = 'http://localhost:3000/api/v1/'
+
+
+export default {
+  token :localStorage.getItem('smart_token') || null,
+  /**
+   * Retrieve list of entities from server using AJAX call.
+   *
+   * @returns {Promise} - Result of ajax call.
+   */
+  fetchEntities(endpoint) {
+    return request({
+      method: 'GET',
+      url: BASE_URL+endpoint,
+      headers: { 'Authorization': `${this.token}` },
+      responseType: 'json',
+    });
+  },
+
+  /**
+   * Submit new entity to server using AJAX call.
+   *
+   * @param {Object} entity - Request body to post.
+   * @returns {Promise} - Result of ajax call.
+   */
+  submitEntity(entity) {
+    return request({
+      method: 'POST',
+      url: BASE_URL,
+      responseType: 'json',
+      headers: { 'Authorization': `${this.token}` },
+      data: entity,
+    });
+  },
+
+};
