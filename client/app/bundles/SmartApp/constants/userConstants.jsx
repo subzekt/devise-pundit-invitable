0d2dcfbcb9197b9a92deb480d61f7bf1b809c790
sarojkh <alt.sarojk@gmail.com>
2017-03-08 12:32:45 +0545

    added action for user creation

---
+++ client/app/bundles/SmartApp/constants/userConstants.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+
+export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const REQUEST_USERS = 'REQUEST_USERS'
+export const SUBMIT_USER_SUCCESS = 'SUBMIT_USER_SUCCESS';
+export const SUBMIT_USER_FAILURE = 'SUBMIT_USER_FAILURE';
+export const SET_IS_SAVING = 'SET_IS_SAVING';
