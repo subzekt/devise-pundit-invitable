cbf75a3ac051ece79dfea7e42a5060d060722e54
subas45 <poudel.subas089@gmail.com>
2017-03-18 22:17:20 +0545

    finally way to make material ui work in tests

--- client/app/bundles/SmartApp/components/users/index.spec.jsx
+++ client/app/bundles/SmartApp/components/users/index.spec.jsx
@@ -4,16 +4,25 @@ import User from './user';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import Paper from 'material-ui/Paper';
+import Layout from '../layout/Layout'
 
 const {
   renderIntoDocument,
-  findRenderedDOMComponentWithTag,
   scryRenderedComponentsWithType,
-  findAllInRenderedTree
 } = TestUtils;
 
 describe('UserList', () => {
+  const mountWithContext = (node) => mount(node, {
+    context: {
+      muiTheme: getMuiTheme(),
+    },
+    childContextTypes: {
+      muiTheme: React.PropTypes.object.isRequired,
+    }
+  });
+
   const users = [
     {
       id: 1,
@@ -23,36 +32,31 @@ describe('UserList', () => {
     },
     {
       id: 2,
-      username: "test",
-      email: "test@example.com",
+      username: "tester",
+      email: "tester@example.com",
       temp_password: ''
     }
   ];
   injectTapEventPlugin();
-  const component = renderIntoDocument(
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <UserList users={users} page={1} pages={1}  />
-    </MuiThemeProvider>
 
-  );
+  const wrapper = mountWithContext(<UserList users={users} page={1} pages={1} />);
 
   it('renders user list component', () => {
-    const list = scryRenderedComponentsWithType(component, UserList);
+    const list = wrapper.find(UserList);
     expect(1).to.equal(list.length);
   })
 
-  // it('renders a list of users in normal order', () => {
-  //   const list = scryRenderedComponentsWithType(component, User);
-  //   expect(1).to.equal(list.length);
-  //   expect(list[0].props.user.username).to.equal('test');
-  //   expect(list[0].props.user.email).to.equal('test@example.com');
-  // });
 
   it('renders a list of users', () => {
-    const wrapper = shallow(
-      <UserList users={users} page={1} pages={1}  />);
     const list =  wrapper.find(User);
     expect(list).to.have.length(2);
+    console.log(list.nodes[0].props.user.username);
+    expect(list.nodes[0].props.user.username).to.equal('test');
+    expect(list.nodes[0].props.user.email).to.equal('test@example.com');
+    expect(list.nodes[1].props.user.email).to.equal('tester@example.com');
+    expect(list.nodes[1].props.user.username).to.equal('tester');
+
   })
+
 });
 
