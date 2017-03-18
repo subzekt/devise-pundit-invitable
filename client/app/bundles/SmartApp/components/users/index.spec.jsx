8afdc0885023e8527ea40c885617cc6de6d71135
subas45 <poudel.subas089@gmail.com>
2017-03-18 17:52:24 +0545

    added enzyme

--- client/app/bundles/SmartApp/components/users/index.spec.jsx
+++ client/app/bundles/SmartApp/components/users/index.spec.jsx
@@ -4,6 +4,7 @@ import User from './user';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { shallow } from 'enzyme';
 
 const {
   renderIntoDocument,
@@ -16,6 +17,12 @@ describe('UserList', () => {
   const users = [
     {
       id: 1,
+      username: "test",
+      email: "test@example.com",
+      temp_password: ''
+    },
+    {
+      id: 2,
       username: "test",
       email: "test@example.com",
       temp_password: ''
@@ -34,10 +41,18 @@ describe('UserList', () => {
     expect(1).to.equal(list.length);
   })
 
-  it('renders a list of users in normal order', () => {
-    const list = scryRenderedComponentsWithType(component, User);
-    expect(1).to.equal(list.length);
-    expect(list[0].props.user.username).to.equal('test');
-    expect(list[0].props.user.email).to.equal('test@example.com');
-  });
+  // it('renders a list of users in normal order', () => {
+  //   const list = scryRenderedComponentsWithType(component, User);
+  //   expect(1).to.equal(list.length);
+  //   expect(list[0].props.user.username).to.equal('test');
+  //   expect(list[0].props.user.email).to.equal('test@example.com');
+  // });
+
+  it('renders a list of users', () => {
+    const wrapper = shallow(
+      <UserList users={users} page={1} pages={1}  />);
+    const list =  wrapper.find(User);
+    expect(list).to.have.length(2);
+  })
 });
+
--- client/app/bundles/SmartApp/components/users/user.jsx
+++ client/app/bundles/SmartApp/components/users/user.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
-// const User = ({user}) => (
-//   <TableRow>
-//     <TableRowColumn>{user.username}</TableRowColumn>
-//     <TableRowColumn>{user.email}</TableRowColumn>
-//     <TableRowColumn>{user.temp_password}</TableRowColumn>
-//   </TableRow>
-// );
-// export default User;
-export default class extends React.Component {
-  render() {
-    const {user} = this.props;
-    return (
-      <TableRow>
-        <TableRowColumn>{user.username}</TableRowColumn>
-        <TableRowColumn>{user.email}</TableRowColumn>
-        <TableRowColumn>{user.temp_password}</TableRowColumn>
-      </TableRow>
-    )
-  }
-}
+const User = ({user}) => (
+  <TableRow>
+    <TableRowColumn>{user.username}</TableRowColumn>
+    <TableRowColumn>{user.email}</TableRowColumn>
+    <TableRowColumn>{user.temp_password}</TableRowColumn>
+  </TableRow>
+);
+export default User;
+// export default class extends React.Component {
+//   render() {
+//     const {user} = this.props;
+//     return (
+//       <TableRow>
+//         <TableRowColumn>{user.username}</TableRowColumn>
+//         <TableRowColumn>{user.email}</TableRowColumn>
+//         <TableRowColumn>{user.temp_password}</TableRowColumn>
+//       </TableRow>
+//     )
+//   }
+// }
