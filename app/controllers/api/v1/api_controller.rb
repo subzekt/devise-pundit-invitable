020e74193ed29f1a49068a1af819aeef6975f7dd
subas45 <poudel.subas089@gmail.com>
2017-06-17 13:23:35 +0545

    updated the api controller to inherit from ApplicationController::API

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,35 @@
 module Api
   module V1
-    class ApiController < ApplicationController
+    class ApiController <  ActionController::API
       respond_to :json
+      include Pundit
+      # Callbacks
+      before_action :authenticate_user!, :unless => :devise_controller?
+      before_action :configure_permitted_parameters, if: :devise_controller?
+      before_action :setup_user_for_system, :unless => :devise_controller?
+      # pundit authorization skip for devise controllers
+      after_action :verify_authorized, :unless => :devise_controller?
+      rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+      private
+
+      def user_not_authorized
+        render :json => {:success => false, :message => 'Permission denied'}, :status => 403
+      end
+
+      def configure_permitted_parameters
+        added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      end
+
+      def setup_user_for_system
+        # check if the user has temp password
+        # if yes force user to change password
+        if user_signed_in? && current_user.temp_password.present?
+          redirect_to edit_user_registration_path, alert: "You must change your password before logging in for the first time"
+        end
+      end
     end
   end
 end
